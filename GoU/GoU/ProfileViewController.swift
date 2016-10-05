1e77a46e81714087e4415d6db84aac0174400b14
yufansun <yufansun@umich.edu>
2016-10-05 14:25:03 -0400

    basic page

---
+++ GoU/GoU/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  GoU
+//
+//  Created by SunYufan on 10/4/16.
+//  Copyright © 2016 SunYufan. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GoU
+GoU is a prototype car pooling app in iOS
+
