137b9c1f08870a856e45e3a5fb4c8fd8fe3bbd38
yufansun <yufansun@umich.edu>
2016-10-09 21:31:46 -0400

    remove Pods folder

--- Pods/Target Support Files/Pods-GoU/Pods-GoU-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoU : NSObject
-@end
-@implementation PodsDummy_Pods_GoU
-@end
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GoU
 GoU is a prototype car pooling app in iOS
 
+* Please run "pod update" the first time you run this project
+
