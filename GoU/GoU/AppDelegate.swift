21c19bc72cc4bef42c370bebd3aab4ec93e42ffd
Chenhao Li <lchenhao@umich.edu>
2016-10-23 23:07:05 -0400

    common profile struct and constructor

--- GoU/GoU/AppDelegate.swift
+++ GoU/GoU/AppDelegate.swift
@@ -33,6 +33,95 @@ var trip = Trip(from: "Ann Arbor",to: "Chicago",date: "10/18/2016",seats: "3")
 
 var trips = [Trip]()
 
+struct Location {
+    //TODO: make it a enum for location
+    var countryName: String
+    var stateName: String
+    var cityName: String
+    
+    init(countryName: String, stateName: String, cityName: String) {
+        self.countryName = countryName
+        self.stateName = stateName
+        self.cityName = cityName
+    }
+    mutating func setNew(countryName: String, stateName: String, cityName: String) {
+        self.countryName = countryName
+        self.stateName = stateName
+        self.cityName = cityName
+    }
+}
+
+struct ContactInformation {
+    var emailAddress: String
+    var phoneNumber: String
+    
+    init(emailAddress: String, phoneNumber: String) {
+        self.emailAddress = emailAddress
+        self.phoneNumber = phoneNumber
+    }
+    mutating func setNew(emailAddress: String, phoneNumber: String) {
+        self.emailAddress = emailAddress
+        self.phoneNumber = phoneNumber
+    }
+}
+
+
+struct CommonProfile {
+    var lastName: String
+    var firstName: String
+    var gender: String //TODO: make it a enum
+    var birthDate: String
+    var contactInfo: ContactInformation;
+    var currentLocation: Location
+    var homeLocation: Location
+    var aboutMe: String
+    var schoolName: String //TODO: make it a enum
+    var majorField: [String] //TODO: make it a enum
+    var languages: [String] //TODO: make it a enum
+    //TODO: more habit information
+    
+
+    init(lastName: String, firstName: String, gender: String, birthDate: String, contactInfo: ContactInformation, currentLocation: Location, homeLocation: Location, aboutMe: String, schoolName: String, majorField: [String], languages: [String]) {
+        self.lastName = lastName
+        self.firstName = firstName
+        self.gender = gender
+        self.birthDate = birthDate
+        self.contactInfo = contactInfo
+        self.currentLocation = currentLocation
+        self.homeLocation = homeLocation
+        self.aboutMe = aboutMe
+        self.schoolName = schoolName
+        self.majorField = majorField
+        self.languages = languages
+    }
+    mutating func setNew(lastName: String, firstName: String, gender: String, birthDate: String, contactInfo: ContactInformation, currentLocation: Location, homeLocation: Location, aboutMe: String, schoolName: String, majorField: [String], languages: [String]) {
+        self.lastName = lastName
+        self.firstName = firstName
+        self.gender = gender
+        self.birthDate = birthDate
+        self.contactInfo = contactInfo
+        self.currentLocation = currentLocation
+        self.homeLocation = homeLocation
+        self.aboutMe = aboutMe
+        self.schoolName = schoolName
+        self.majorField = majorField
+        self.languages = languages
+    }
+}
+
+//mock data 
+var loc = Location(countryName: "US", stateName: "Michigan", cityName: "Ann Arbor")
+
+var contactInfo = ContactInformation(emailAddress: "lchenhao@umich.edu", phoneNumber: "+1-734-780-9882")
+
+var majors = ["CS", "EE"]
+
+var lan = ["English", "Chinese"]
+
+
+var sampleProfile = CommonProfile(lastName: "Li", firstName: "Chenhao", gender: "Male", birthDate: "02/21/1995", contactInfo: contactInfo, currentLocation: loc, homeLocation: loc, aboutMe: "GoU is the best", schoolName: "University of Michigan", majorField: majors, languages: lan)
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
