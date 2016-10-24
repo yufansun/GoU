cd44ab04f60cf41c99cc9356ea4afc7e420fd253
yufansun <yufansun@umich.edu>
2016-10-23 23:44:28 -0400

    link front and back of profile view

--- GoU/GoU/ProfileViewController.swift
+++ GoU/GoU/ProfileViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 import Firebase
 
 class ProfileViewController: UIViewController {
+    @IBOutlet weak var firstnameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var currentCountryTextField: UITextField!
+    @IBOutlet weak var currentStateTextField: UITextField!
+    @IBOutlet weak var currentCityTextField: UITextField!
+    @IBOutlet weak var usernameLabel: UILabel!
     // MARK: Properties
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var email = ""
+        if let user = FIRAuth.auth()?.currentUser {
+            var name = user.displayName
+            email = user.email!
+            var photoUrl = user.photoURL
+            var uid = user.uid;  // The user's ID, unique to the Firebase project.
+        } else {
+            // No user is signed in.
+        }
+        print(email)
+        var myStr = "hah"
+        usernameLabel.text = email
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +54,34 @@ class ProfileViewController: UIViewController {
         } catch let signOutError as NSError {
             print ("Error signing out: \(signOutError.localizedDescription)")
         }
+    }
+    
+    @IBAction func didTapSave(_ sender: AnyObject) {
+        if (firstnameTextField.text! == "" || lastNameTextField.text! == "" ||
+            phoneTextField.text! == "" || currentCountryTextField.text! == "" || currentStateTextField.text! == "" || currentCityTextField.text! == "") {
+            showAlert()
+        } else {
+            let contactInfo = ContactInformation.init(emailAddress: "test.umich.edu", phoneNumber: phoneTextField.text!)
+            let firstName = firstnameTextField.text!
+            let lastName = lastNameTextField.text!
+            let currentLocation = Location.init(countryName: currentCountryTextField.text! , stateName: currentStateTextField.text!, cityName: currentCityTextField.text!)
+            CommonProfile.init(lastName: lastName, firstName: firstName, gender: "female", birthDate: "01/01/1995", contactInfo: contactInfo, currentLocation: currentLocation, homeLocation: currentLocation, aboutMe: "aboutMe", schoolName: "UMich", majorField: ["Computer Science"], languages: ["Chinese","English"])
+            
+            let alert = UIAlertController(title: "Thank You",
+                                          message: "User information updated sucessfully", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Alert",
+                                      message: "Please complete the information", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 }
